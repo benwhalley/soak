6a43e38fb856b3d2d8d92f306866fd86c462cacd
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-10-08 08:30:31 +0100

    make samples, tweak pipes

---
+++ docs/samples/make-samples.sh
@@ -0,0 +1,5 @@
+uv run soak -v run zs data/cfs/\*.txt -o cfs1 --model-name="litellm/gpt-4.1-mini"  -t simple -t pipeline
+
+uv run soak -v run zs data/cfs/\*.txt -o cfs2 --model-name="litellm/gpt-4.1"  -t simple -t pipeline
+
+uv run soak compare -v cfs1.json cfs2.json --output=cfs_comparison.html 
